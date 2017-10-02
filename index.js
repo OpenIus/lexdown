b953ff0331f0122faa2fce6d4aa1fc81d397999c
Max Gfeller <max.gfeller@gmail.com>
2017-10-02 17:53:27 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const fs = require('fs')
+const remark = require('remark')
+const html = require('remark-html')
+const frontmatter = require('remark-frontmatter')
+const parse = require('remark-parse')
+
+var contents = fs.readFileSync(path.join(__dirname, 'test.lexdown'), { encoding: 'utf8' })
+
+remark()
+  .use(parse)
+  .use(frontmatter, ['yaml'])
+  .use(logger)
+  .process(contents, (err, file) => {
+    fs.writeFileSync(path.join(__dirname, 'test.html'), file.contents)
+  })
+
+function logger () {
+  return console.log
+}
