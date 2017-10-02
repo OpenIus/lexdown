479495ab570dd2d6c2daa3ea2f3f9890f7c27aeb
Max Gfeller <max.gfeller@gmail.com>
2017-10-02 19:14:56 +0200

    Make footnotes work

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const remark = require('remark')
 const html = require('remark-html')
 const frontmatter = require('remark-frontmatter')
 const parse = require('remark-parse')
+const unified = require('unified')
 
 function logger () {
   return console.log
 }
 
 function parseLexdown (contents, opts, cb) {
-  remark({ useFootnotes: true })
-    .use(parse)
+  unified()
+    .use(parse, { footnotes: true })
     .use(frontmatter, ['yaml'])
     .use(html)
     .process(contents, (err, file) => {
