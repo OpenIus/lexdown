1294a9cb6f22b89917986f2513893022eeb3a835
Max Gfeller <max.gfeller@gmail.com>
2017-10-02 23:30:03 +0200

    Add support for marginalia
    Syntax looks like this:
    
    [[This is a marginalia]]

--- index.js
+++ index.js
@@ -5,12 +5,43 @@ const parse = require('remark-parse')
 const unified = require('unified')
 
 function logger () {
-  return console.log
+  return function (data) {
+    console.log(JSON.stringify(data))
+  }
+}
+
+function locateMarginalia (value, fromIndex) {
+  return value.indexOf('[[', fromIndex)
+}
+
+tokenizeMarginalia.notInLink = true
+tokenizeMarginalia.locator = locateMarginalia
+function tokenizeMarginalia (eat, value, silent) {
+  var match = /^\[\[([ \w+]*)\]\]/.exec(value)
+
+  if (!match) return
+  if (silent) return true
+
+  return eat(match[0])({
+    type: 'html',
+    value: `<div class="marginalia">${match[1]}</div>`
+  })
+}
+
+function marginalia () {
+  var Parser = this.Parser
+  var tokenizers = Parser.prototype.inlineTokenizers
+  var methods = Parser.prototype.inlineMethods
+
+  tokenizers.marginalia = tokenizeMarginalia
+  methods.splice(methods.indexOf('text'), 0, 'marginalia')
 }
 
 function parseLexdown (contents, opts, cb) {
   unified()
     .use(parse, { footnotes: true })
+    .use(marginalia)
+    // .use(logger)
     .use(frontmatter, ['yaml'])
     .use(html)
     .process(contents, (err, file) => {
