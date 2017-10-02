947904ebef1e4221fd49bca23a140d33bf522069
Max Gfeller <max.gfeller@gmail.com>
2017-10-02 18:03:45 +0200

    Generate html document

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const html = require('remark-html')
 const frontmatter = require('remark-frontmatter')
 const parse = require('remark-parse')
 
-var contents = fs.readFileSync(path.join(__dirname, 'test.lexdown'), { encoding: 'utf8' })
+var contents = fs.readFileSync(path.join(__dirname, 'test.lexdown'), {
+  encoding: 'utf8'
+})
 
 remark()
   .use(parse)
   .use(frontmatter, ['yaml'])
+  .use(html)
   .use(logger)
   .process(contents, (err, file) => {
     fs.writeFileSync(path.join(__dirname, 'test.html'), file.contents)
