76da5f7a3dcf85d3134303e411a196f0bfb48b3b
Max Gfeller <max.gfeller@gmail.com>
2017-10-03 11:26:15 +0200

    Use reflinks

--- index.js
+++ index.js
@@ -7,37 +7,13 @@ const parse = require('remark-parse')
 const unified = require('unified')
 const yaml = require('js-yaml')
 
+const marginalia = require('./syntax/marginalia')
+const reflink = require('./syntax/reflink')
+
 function logger () {
   return function (data) {
     // console.log(JSON.stringify(data))
   }
-}
-
-function locateMarginalia (value, fromIndex) {
-  return value.indexOf('[[', fromIndex)
-}
-
-tokenizeMarginalia.notInLink = true
-tokenizeMarginalia.locator = locateMarginalia
-function tokenizeMarginalia (eat, value, silent) {
-  var match = /^\[\[([ \w+]*)\]\]/.exec(value)
-
-  if (!match) return
-  if (silent) return true
-
-  return eat(match[0])({
-    type: 'html',
-    value: `<div class="marginalia">${match[1]}</div>`
-  })
-}
-
-function marginalia () {
-  var Parser = this.Parser
-  var tokenizers = Parser.prototype.inlineTokenizers
-  var methods = Parser.prototype.inlineMethods
-
-  tokenizers.marginalia = tokenizeMarginalia
-  methods.splice(methods.indexOf('text'), 0, 'marginalia')
 }
 
 var docTitle = null
@@ -59,6 +35,7 @@ function parseLexdown (contents, opts, cb) {
   unified()
     .use(parse, { footnotes: true })
     .use(marginalia)
+    .use(reflink)
     .use(parseMetadata)
     .use(logger)
     .use(frontmatter, ['yaml'])
