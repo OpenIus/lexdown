dc227e7445c166cea2f59e3f8379c42472d21b7b
Nicolas Degen <nicolas@kapanu.com>
2017-10-03 13:21:22 +0200

    added contract

---
+++ cod.sh
@@ -0,0 +1,2 @@
+mkdir -p html
+bin/parser.js examples/cod/test.lexdown > html/test.html && open html/test.html
---
+++ contract.sh
@@ -0,0 +1,2 @@
+mkdir -p html
+bin/parser.js examples/contract/hpa.lexdown > html/hpa.html && open html/hpa.html
