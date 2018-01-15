3ad706799fd62ca06d1714a5750ddf7c58402f1e
Nicolas Degen <ndegen@me.com>
2018-01-15 22:32:52 +0100

    get prefix when iterating over dirs

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-function loop() { 
+function loop() {
+  original="$2"
     for i in "$1"/*
     do
         if [ -d "$i" ]; then
-            loop "$i"
+            loop "$i" "$2"
         elif [ -e "$i" ]; then
             echo $i
+            # foo=${i#$prefix}
+            # foo=${foo%$suffix}
+            # echo $original
+            tmp=${i#$original}
+            echo $tmp
             # wkhtmltopdf tst.html test.pdf
             # pandoc  -s -smart tst.html -o test.pdf
         else
@@ -14,4 +20,11 @@ function loop() {
     done
 }
 
-loop "$PWD"
+loop "$PWD" "$PWD"
+
+
+# for file in */ ; do 
+#   if [[ -d "$file" && ! -L "$file" ]]; then # ignore symlinks and dirs
+#     echo "$file is a directory"; 
+#   fi; 
+# done
